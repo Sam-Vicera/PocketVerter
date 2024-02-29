d403154836046905af9810e8a2adac4a3f1deade
Samuel Hernandez Vicera <shernandezvicera@apple.com>
2024-02-29 10:10:31 -0700

    Initial Commit

---
+++ PocketVerter/PocketVerterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketVerterApp.swift
+//  PocketVerter
+//
+//  Created by Samuel Hernandez Vicera on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PocketVerterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
