d69647b3ba173389f7778b0b7a33088bccdef8bc
Samuel Hernandez Vicera <shernandezvicera@gmail.com>
2024-06-06 15:21:46 -0700

    Project Completed

--- PocketVerter/ContentView.swift
+++ PocketVerter/ContentView.swift
@@ -8,14 +8,97 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var initialValue = 0
+    @State private var initialUnit = "Fahrenheit"
+    @State private var convertedUnit = "Fahrenheit"
+    @State private var convertedValue = 0
+    
+    @FocusState private var amountIsFocused: Bool
+
+    
+    var tempConversion : Double {
+        let initialUnit = initialUnit
+        var initialValue = Double(initialValue)
+        
+        switch initialUnit{
+        case "Fahrenheit":
+            if convertedUnit == initialUnit {
+                return initialValue
+            }
+            else if convertedUnit == "Celsius"{
+                return (initialValue - 32) * (5/9)
+            }
+            else { return (initialValue + 459.67 ) / 1.8 } // kelvin conversion
+       
+        case "Celsius":
+            if convertedUnit == initialUnit {
+                return initialValue
+            }
+            else if convertedUnit == "Fahrenheit"{
+                return (1.8 * initialValue) + 32
+            }
+            else {return (initialValue + 273.15)} // kelvin conversion
+       
+        case "Kelvin":
+            if convertedUnit == initialUnit {
+                return initialValue
+            }
+            else if convertedUnit == "Fahrenheit"{
+                return (1.8 * (initialValue - 273) + 32)
+            }
+            else { return initialValue - 273.15} // celcius conversion
+            
+        default:
+        print("Something likely went wrong")
+        
+            
         }
-        .padding()
+        
+        return 0
+        
+    }
+    
+    let possibleUnits = ["Fahrenheit", "Celsius", "Kelvin"]
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section{
+                    Picker("Initial Unit", selection: $initialUnit){
+                        ForEach(possibleUnits, id:\.self){
+                         Text($0)
+                        }
+                    }
+                }
+                
+                Section("Value You Would Like Converted"){
+                    TextField("Value", value: $initialValue, format: .number)
+                }
+                .keyboardType(.decimalPad)
+                .focused($amountIsFocused)
+                
+                Section{
+                    Picker("Desired Unit", selection: $convertedUnit){
+                        ForEach(possibleUnits, id:\.self){
+                         Text($0)
+                        }
+                    }
+                }
+                
+                Section{
+                    Text("Your converted value in \(convertedUnit): \(String(format: "%.2f", tempConversion))")
+                }
+            }
+            .navigationTitle("PocketVerter")
+            .toolbar(){
+                if amountIsFocused {
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+        
     }
 }
 
