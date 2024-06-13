f3b695f342a5d693c23bd5d23c2c5f2027ecf394
Samuel Hernandez Vicera <shernandezvicera@gmail.com>
2024-06-13 13:31:55 -0700

    SetUp for landing page

---
+++ PocketVerter/LandingPageView.swift
@@ -0,0 +1,24 @@
+//
+//  LandingPageView.swift
+//  PocketVerter
+//
+//  Created by Samuel Hernandez Vicera on 6/13/24.
+//
+
+import SwiftUI
+
+struct LandingPageView: View {
+    var body: some View {
+        NavigationView{
+            HStack{
+                
+            }
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .navigationTitle("Placeholder")
+    }
+}
+
+#Preview {
+    LandingPageView()
+}
