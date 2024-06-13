96d9f8569fa3e9693d8697c2ae7b52077c0a5ec9
Samuel Hernandez Vicera <shernandezvicera@gmail.com>
2024-06-13 15:40:19 -0700

    Wireframe for landing page complete

--- PocketVerter/LandingPageView.swift
+++ PocketVerter/LandingPageView.swift
@@ -9,15 +9,90 @@ import SwiftUI
 
 struct LandingPageView: View {
     var body: some View {
-        NavigationView{
-            HStack{
+        NavigationStack {
+            VStack{
                 
+                
+                VStack(alignment: .center ,spacing: 65){
+                    Text("PocketVerter")
+                        .foregroundStyle(.white)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    
+                    Image("Delta")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .background(Color.gray)
+                }
+                Spacer()
+                
+                VStack(spacing: 35){
+                    
+                    Button("Quick Access / Starred") {
+                        print("Button pressed!")
+                    }
+                    .buttonStyle(pocketVerterButtonStyle())
+                    
+                    Button("Time Conversions") {
+                        print("Button pressed!")
+                    }
+                    .buttonStyle(pocketVerterButtonStyle())
+                    
+                    Button("Temperature Conversions") {
+                        print("Button pressed!")
+                    }
+                    .buttonStyle(pocketVerterButtonStyle())
+                    
+                    Button("Distance Conversions") {
+                        print("Button pressed!")
+                    }
+                    .buttonStyle(pocketVerterButtonStyle())            }
+             
+                Spacer()
             }
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .frame(minWidth: 500, minHeight: 500)
+            .background(.darkBackground)
+            
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink(destination: EditPreferencesView()) {
+                        Label("Edit user preferences", systemImage: "gear")
+                    }
+                }
+                ToolbarItem(placement: .topBarLeading) {
+                    NavigationLink(destination: EditPreferencesView()) {
+                        Label("Edit user preferences", systemImage: "magnifyingglass")
+                    }
+                }
+            }
         }
-        .navigationTitle("Placeholder")
+        
+        
+    }
+        
+}
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 1/255, green: 6/255, blue: 28/255)
+    }
+    
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
     }
 }
+struct pocketVerterButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: 250)
+            .padding()
+            .foregroundStyle(.white)
+            .background(Color(red: 0, green: 0.3, blue: 0.5))
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.4 : 1)
+            .animation(.easeOut(duration: 0.3), value: configuration.isPressed)
+    }
+}
+
 
 #Preview {
     LandingPageView()
